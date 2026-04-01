1566030538bd3d5ceba0a9166e51ec3890e16d78
Millieon Hu <38780746+Millieon@users.noreply.github.com>
2026-04-02 00:11:36 +0800

    npm deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // If embedding on a landing page as a sub-path:
+  plugins: [react()],
   // base: '/demo/',
+  base: '/nuzzle-demo/',
 })
